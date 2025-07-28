72c6b905ee6e09910aec8e26f42ea86b04406b1d
lam thanh qui <lamthanhquihgs@gmail.com>
2025-07-28 11:17:05 +0700

    buil

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Lâm thanh quí</h1>
+      <h1>Lâm thanh quí 123</h1>
     </div>
   )
 }
