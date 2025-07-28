026f29963de4fdb13a9457b8e449936853bedb24
lam thanh qui <lamthanhquihgs@gmail.com>
2025-07-28 21:03:26 +0700

    push

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,17 @@ import axios from 'axios';
 function App() {
   useEffect(() => {
     const checkForUpdates = async () => {
-      // window.location.reload(true); // Tải lại từ server
       try {
-        // Fetch version từ file tĩnh trên public
         const localResponse = await axios.get('/version.json');
-        console.log(localResponse, window.localStorage.getItem('build-version'))
-        // window.location.reload()
-        if (!localResponse.ok) {
-          throw new Error('Failed to fetch local version');
-        }
-        // console.log(window.localStorage.getItem('build-version'))
+        console.log(localResponse?.data?.version)
         if(window.localStorage.getItem('build-version')){
-          if(localResponse?.version!=window.localStorage.getItem('build-version')){
-              window.localStorage.setItem('build-version', localResponse.version)
+          if(localResponse?.data?.version!=window.localStorage.getItem('build-version')){
+              window.localStorage.setItem('build-version', localResponse?.data?.version)
               window.location.reload()
           }
+        }else{
+          console.log(123)
+          window.localStorage.setItem('build-version', localResponse?.data?.version)
         }
         
       } catch (error) {
@@ -27,13 +23,13 @@ function App() {
     };
 
     checkForUpdates(); // Chạy ngay khi component mount
-    const interval = setInterval(checkForUpdates, 10000); // Kiểm tra mỗi 5 phút
+    const interval = setInterval(checkForUpdates, 10000); 
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div>
-      <h1>React App o</h1>
+      <h1>React App ok</h1>
     </div>
   );
 }
