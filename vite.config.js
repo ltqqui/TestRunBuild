df1701c778d9f1b0310536d6a42920e2fce95d25
lam thanh qui <lamthanhquihgs@gmail.com>
2025-07-28 13:16:22 +0700

    abc

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    __PUSHER_KEY__: JSON.stringify('your-pusher-key'),
-    __PUSHER_CLUSTER__: JSON.stringify('your-cluster')
-  }
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ]
 })
