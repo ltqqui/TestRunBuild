4a042bd0ecdb0ba436195f7400ee636d48e17ec4
lam thanh qui <lamthanhquihgs@gmail.com>
2025-07-28 13:49:33 +0700

    abc

--- hmr-client.js
+++ hmr-client.js
@@ -1,11 +1,11 @@
-// import Pusher from 'pusher-js'
+import Pusher from 'pusher-js'
 
-// const pusher = new Pusher(__PUSHER_KEY__, {
-//   cluster: __PUSHER_CLUSTER__,
-//   encrypted: true
-// })
+const pusher = new Pusher(__PUSHER_KEY__, {
+  cluster: __PUSHER_CLUSTER__,
+  encrypted: true
+})
 
-// const channel = pusher.subscribe('hmr-channel')
-// channel.bind('update', () => {
-//   import.meta.hot.send('vite:update')
-// })
+const channel = pusher.subscribe('hmr-channel')
+channel.bind('update', () => {
+  import.meta.hot.send('vite:update')
+})
