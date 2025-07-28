11768d365e1813e568ba1ab40443c33d3c62b72e
lam thanh qui <lamthanhquihgs@gmail.com>
2025-07-28 15:23:37 +0700

    abc

--- api/version.js
+++ api/version.js
@@ -1,4 +1,4 @@
 // api/version.js
 export default function handler(req, res) {
-  res.status(200).json({ version: process.env.APP_VERSION || '1.0.0' });
+  res.status(200).json({ version: process.env.APP_VERSION || 'hehehe' });
 }
